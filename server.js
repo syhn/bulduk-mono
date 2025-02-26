b94fcac4f4e91631deb42fb3250e559c847d98a7
syhn <seyhan.yagcioglu@gmail.com>
2025-02-26 23:20:43 +0300

    api conf changed https://buld.uk/api/

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const app = express();
+
+// API isteklerini buld.uk backend'ine yönlendir
+app.use("/api", createProxyMiddleware({
+  target: process.env.API_URL || "https://buld.uk",
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '/api'  // Path rewrite gerekirse kullanılabilir
+  }
+}));
+
+// Static dosyaları serve et
+app.use(express.static(path.join(__dirname, "build")));
+
+// Diğer tüm istekleri React'e yönlendir
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
