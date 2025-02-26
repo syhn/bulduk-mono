561bf3836bec7924d8b7f8547cf72b31ca608e35
syhn <seyhan.yagcioglu@gmail.com>
2025-02-26 23:06:33 +0300

    api conf changed https://buld.uk/api/

--- bulduk-be/api/apps/true_false/urls.py
+++ bulduk-be/api/apps/true_false/urls.py
@@ -4,10 +4,10 @@ from .views import start_session, get_question, check_answer, get_session_stats,
 app_name = 'true_false'
 
 urlpatterns = [
-    path('games/true-false/session/start/', start_session, name='start_session'),
-    path('games/true-false/session/end/', end_session, name='end_session'),
-    path('games/true-false/question/', get_question, name='get_question'),
-    path('games/true-false/check/', check_answer, name='check_answer'),
-    path('games/true-false/stats/', get_session_stats, name='get_session_stats'),
-    path('games/true-false/categories/', get_categories, name='get_categories'),
+    path('api/games/true-false/session/start/', start_session, name='start_session'),
+    path('api/games/true-false/session/end/', end_session, name='end_session'),
+    path('api/games/true-false/question/', get_question, name='get_question'),
+    path('api/games/true-false/check/', check_answer, name='check_answer'),
+    path('api/games/true-false/stats/', get_session_stats, name='get_session_stats'),
+    path('api/games/true-false/categories/', get_categories, name='get_categories'),
 ] 
